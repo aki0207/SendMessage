69e77208011083a56cd11d98b64478fd6deebcec
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-22 10:16:51 +0900

    api keyとroom idは空にしてる

--- SendMessage/ViewController.swift
+++ SendMessage/ViewController.swift
@@ -1,20 +1,49 @@
-//
-//  ViewController.swift
-//  SendMessage
-//
-//  Created by ききょ on 2019/03/22.
-//  Copyright © 2019 kikyo. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var api_key = "my api key"
+    var url = "https://api.chatwork.com/v2"
+    var room_id = "my room id"
+    var msg = "できたゾォ"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        hitTheApi()
     }
-
-
+    
+    func hitTheApi() {
+        
+        var urlString = "https://api.chatwork.com/v2"
+        urlString = "\(urlString)/rooms/\(room_id)/messages"
+        let request = NSMutableURLRequest(url: NSURL(string: urlString)! as URL)
+        
+        //set the method(HTTP-POST)
+        request.httpMethod = "POST"
+        //set the header(s)
+        request.addValue(api_key, forHTTPHeaderField: "X-ChatWorkToken")
+        
+        //set the request-body(JSON)
+        let params = "body=\(msg)"
+        
+        
+        do {
+            
+            request.httpBody = params.data(using: .utf8)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
+            if (error == nil) {
+                let result = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
+                print("大成功")
+                print(result)
+            } else {
+                print("oh my god")
+                print(error as Any)
+            }
+        })
+        task.resume()
+    }
 }
-
